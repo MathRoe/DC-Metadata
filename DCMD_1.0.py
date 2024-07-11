035068ded6054769707b14ba027c527118892421
MathRoe <117972306+MathRoe@users.noreply.github.com>
2024-07-11 18:01:15 +0200

    Update DCMD_1.0.py

--- DCMD_1.0.py
+++ DCMD_1.0.py
@@ -5,11 +5,11 @@ from rdflib import Graph, Namespace
 from rdflib.namespace import NamespaceManager
 from rdflib.namespace import DC
 
-df = pd.read_csv('DC_Metadata.csv', encoding = "ISO-8859-1") #read the csv file
+df = pd.read_csv('DC_Metadata.csv', encoding = "ISO-8859-1") #read the csv file localy
 #define the namespaces
 namespace_manager = NamespaceManager(Graph())
 namespace_manager.bind("dc", DC)
-g = to_graph(df, namespace_manager) #transform the csv file in graf
+g = to_graph(df, namespace_manager) #transform the csv file in graph
 
 #create the turtle file
 serialized_rdf = g.serialize("csvtordf.ttl", format='turtle')
